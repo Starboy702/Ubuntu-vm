563d215088a229096fec607d708c12ac81e1a5fa
Starboy702 <157193287+Starboy702@users.noreply.github.com>
2025-06-05 21:05:28 -0600

    Module 1 challenge activity code

---
+++ ca.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get the current day of the month 
+today=$(date +%d)
+
+# Loop through all the files in the current directory
+for file in *; do
+    # Will check if it's a regular file
+    if [[ -f "$file" ]]; then
+        # Gets the last day of the files last modification
+        mod_day=$(date -r "$file" +%d)
+
+        # Compares with today date
+        if [[ "$mod_day" == "$today" ]]; then
+            echo "$file"
+        fi
+    fi
+done
